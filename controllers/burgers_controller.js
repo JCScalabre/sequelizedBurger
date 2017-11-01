71b4208419810cd21d8c4bf15ba3f8d334f947cc
JC Scalabre <jc_scalabre@hotmail.com>
2017-10-31 22:42:51 -0500

    hi

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,41 +1,52 @@
 var express = require("express");
 
 var router = express.Router();
-var burger = require("../models/burger");
+var db = require("../models");
 
-// get route -> index
 router.get("/", function(req, res) {
   res.redirect("/burgers");
 });
 
 router.get("/burgers", function(req, res) {
-  // express callback response by calling burger.selectAllBurger
-  burger.all(function(data) {
-    // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
+  db.Burger.findAll({}).then(function(data) {
     var hbsObject = { burgers: data };
     res.render("index", hbsObject);
-  });
+  })
 });
 
-// post route -> back to index
 router.post("/burgers/create", function(req, res) {
-  // takes the request object using it as input for buger.addBurger
-  burger.create(req.body.burger_name, function(result) {
-    // wrapper for orm.js that using MySQL insert callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
+  console.log("Req. body: ")
+  console.log(req.body)
+  db.Burger.create({
+    burger_name: req.body.burger_name
+  }).then(function() {
     res.redirect("/");
   });
 });
+
+// router.put("/burgers/rating", function(req, res) {
+//   db.Burger.update({
+//     rating: req.body.rating
+//   }, {
+//     where: {
+//       id: req.body.burger_id
+//     }
+//   }).then(function(r) {
+//     res.json(r)
+//   })
+// })
 
 // put route -> back to index
 router.put("/burgers/update", function(req, res) {
-  burger.update(req.body.burger_id, function(result) {
-    // wrapper for orm.js that using MySQL update callback will return a log to console,
-    // render back to index with handle
-    console.log(result);
-    res.redirect("/");
-  });
+  db.Burger.update({
+    devoured: true
+  }, {
+    where: {
+      id: req.body.burger_id
+    }
+  }).then(function() {
+    res.redirect("/")
+  })
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,16 +7,21 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
 // override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({
-  defaultLayout: "main"
-}));
+app.engine(
+	"handlebars",
+	exphbs({
+		defaultLayout: "main"
+	})
+);
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller");
@@ -25,6 +30,11 @@ app.use("/", routes);
 app.use("/update", routes);
 app.use("/create", routes);
 
-// listen on port 3000
+var db = require("./models");
 var port = process.env.PORT || 3000;
-app.listen(port);
+
+db.sequelize.sync().then(function() {
+	app.listen(port, function() {
+		console.log("App listening on PORT " + port);
+	});
+});
